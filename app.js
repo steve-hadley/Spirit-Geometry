177c9695f084a003ebb7b96c17a8f06a5229b5cf
Steve Hadley <steven.dale.hadley@gmail.com>
2021-10-12 17:04:08 +1100

    Making line resolution and step variable

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-let theta = 0;
-let axis = 6;
-let step;
+var circleElement;
+var lineElement;
 
-// Circles
-let startRadius = 100;
-let radius;
-let startCircleDiameter = 200;
-let circleDiameter;
-
-// Lines
-let lineLength = 400;
-let lineStep = 30;
-
+var controls;
 let axisSlider;
 let radiusSlider;
 let circleDiameterSlider;
 let lineLengthSlider;
 let lineStepSlider;
-
-var controls;
 
 function setup() {
   var canvas = createCanvas();
@@ -31,42 +19,73 @@ function setup() {
   noFill();
   stroke(255);
   strokeWeight(3);
-  radius = startRadius;
-  circleDiameter = startCircleDiameter;
+
+  circleElement = new CircleElement();
+  lineElement = new LineElement();
 }
 
 function draw(){
   background(0);
-
-  step = TWO_PI / axis;
-  axis = controls.querySelector('#axis-slider').value;
-  circleDiameter = controls.querySelector('#circle-diameter-slider').value;
-  radius = controls.querySelector('#radius-slider').value;
-  lineLength = controls.querySelector('#line-length-slider').value;
-
   translate(width / 2, height / 2);
-  theta = 0;
+  circleElement.resize();
+  circleElement.display();
+  lineElement.resize();
+  lineElement.display();
+}
 
-  // Flower
-  circle(0, 0, 200);
-  let circlePos = createVector();
-  for (let i = 0; i < axis; i++) {
-    theta += step;
-    // Polar to cartesian coordinate conversion
-    circlePos.x = radius * cos(theta);
-    circlePos.y = radius * sin(theta);
-    circle(circlePos.x, circlePos.y, circleDiameter);
+class CircleElement{
+  constructor(){
+    this.theta = 0;
+    this.axis = 6;
+    this.step = TWO_PI / this.axis;
+    this.radius;
+    this.diameter;
   }
-
-  // Lines
-  for (let a = 0; a < 360; a += lineStep){
-    push();
-    rotate(radians(a));
-    for (let r = 0; r < 180; r += lineStep) {
-      line(sin(radians(r)) * lineLength, cos(radians(r)) * lineLength, sin(radians(-r)) * lineLength, cos(radians(-r)) * lineLength);
+  resize(){
+    this.axis = GetInput('#axis-slider');
+    this.step = TWO_PI / this.axis;
+    this.diameter = GetInput('#circle-diameter-slider');
+    this.radius = GetInput('#radius-slider');
+  }
+  display(){
+    this.theta = 0;
+    circle(0, 0, 200);
+    let pos = createVector();
+    for (let i = 0; i < this.axis; i++) {
+      this.theta += this.step;
+      // Polar to cartesian coordinate conversion
+      pos.x = this.radius * cos(this.theta);
+      pos.y = this.radius * sin(this.theta);
+      circle(pos.x, pos.y, this.diameter);
     }
-    pop();  
   }
+}
+
+class LineElement{
+  constructor(){
+    this.length = 300;
+    this.step = 40;
+    this.gap = 40;
+  }
+  resize(){
+    this.length = GetInput('#line-length-slider');
+    this.step = GetInput('#line-step-slider');
+    this.gap = GetInput('#line-gap-slider');
+  }
+  display(){
+    for (let a = 0; a < 360; a += this.step){
+      push();
+      rotate(radians(a));
+      for (let r = 0; r < 180; r += this.gap) {
+        line(sin(radians(r)) * this.length, cos(radians(r)) * this.length, sin(radians(-r)) * this.length, cos(radians(-r)) * this.length);
+      }
+      pop();  
+    }
+  }
+}
+
+function GetInput(id){
+  return parseInt(controls.querySelector(id).value);
 }
 
 // Save image
