7b35381549144052f2c1763a4e9bccd0cf3a39dd
Steve Hadley <steven.dale.hadley@gmail.com>
2021-10-15 13:33:26 +1100

    Added animation

--- app.js
+++ app.js
@@ -3,64 +3,107 @@
 // https://vectr.is/links/
 
 // Objects
-let circleElement;
-let lineElement;
+let circleElements = [];
+let lineElements = [];
 
-// Input container
+// Input
 let inputContainer;
-
-// Inputs
 let axisSlider;
 let radiusSlider;
 let circleDiameterSlider;
 let lineLengthSlider;
 let lineStepSlider;
+let lineGapSlider;
+let strokeWeightSlider;
+
+let automatic = true
+
+let timer = 0;
+
+let interval = 4000;
+
+let circleMaxDiameter = 800;
 
 function setup() {
-  inputContainer = document.getElementById('controls');
-
   var canvas = createCanvas();
   canvas.parent('p5js-container');
+  colorMode(RGB, 255, 255, 255, 1);
 
+  // Initialize inputs
+  initInputs();
   // Calculate canvas size in order to adapt to screen size
   calculateCanvasSize();
-  // Set default values
-  setDefaults();
-  // Hook up export button
-  document.getElementById('export-button').onclick = (function () {
-    Export();
-  })
   
   noFill();
   stroke(255);
   strokeWeight(3);
-  
-  // Create objects
-  circleElement = new CircleElement();
-  lineElement = new LineElement();
+
+  var circleElement = new CircleElement();
+  if(automatic){
+    circleElement.diameter = 0;
+  }
+  circleElements.push(circleElement);
+  var lineElement = new LineElement();
+  lineElements.push(lineElement);
 }
 
 function draw(){
   // Main loop
   background(0);
   translate(width / 2, height / 2);
-  strokeWeight(parseInt(GetInputValue('#stroke-weight-slider')));
-  circleElement.resize();
-  circleElement.display();
-  lineElement.resize();
-  lineElement.display();
+
+  // Animate
+  if(automatic){
+    // Every x seconds
+    if(millis() >= interval + timer){
+      // Spawn new circle
+      var circleElement = new CircleElement();
+      circleElement.diameter = 0;
+      circleElements.push(circleElement);
+      timer = millis();
+    }
+
+    for (let i = 0; i < circleElements.length; i++) {
+      circleElements[i].expand();
+      if(circleElements[i].diameter >= circleMaxDiameter){
+        circleElements.splice(i, 1);
+      }
+    }
+
+  }
+
+  // Display
+  circleElements.forEach(circleElement => {
+    circleElement.display();
+  });
+
+  lineElements.forEach(lineElement => {
+    lineElement.display();
+  });
+
 }
+
 class CircleElement{
   constructor(){
     this.resize();
   }
   resize(){
-    this.axis = GetInputValue('#axis-slider');
+    this.axis = GetValue(axisSlider);
     this.step = TWO_PI / this.axis;
-    this.diameter = GetInputValue('#circle-diameter-slider');
-    this.radius = GetInputValue('#radius-slider');
+    this.diameter = GetValue(circleDiameterSlider);
+    this.radius = GetValue(radiusSlider);
+    this.alpha = 1;
+  }
+  expand(){
+    this.diameter += 1;
+  }
+  reset(){
+    this.diameter = 0;
   }
   display(){
+    let alpha = 1.0 - (this.diameter / circleMaxDiameter);
+    strokeWeight(4);
+    stroke(255, 255, 255, alpha);
     // Draw center circle
     circle(0, 0, this.diameter);
     // Angle to rotate by
@@ -83,9 +126,9 @@ class LineElement{
     this.resize();
   }
   resize(){
-    this.length = GetInputValue('#line-length-slider');
-    this.outerStep = GetInputValue('#line-step-slider');
-    this.innerStep = GetInputValue('#line-gap-slider');
+    this.length = GetValue(lineLengthSlider);
+    this.outerStep = GetValue(lineStepSlider);
+    this.innerStep = GetValue(lineGapSlider);
   }
   display(){
     // Rotate by the chosen step
@@ -95,6 +138,8 @@ class LineElement{
       // Draw lines in a wave pattern with a variable dispersion
       for (let r = 0; r < 180; r += this.innerStep) {
         // Rad method of creating line lengths in a wave pattern, inspired by https://linktr.ee/thedotiswhite
+        strokeWeight(2);
+        stroke(255, 255, 255, 1);
         line(sin(radians(r)) * this.length, cos(radians(r)) * this.length, sin(radians(-r)) * this.length, cos(radians(-r)) * this.length);
       }
       pop();  
@@ -107,38 +152,65 @@ function Export() {
   saveCanvas('Export', 'jpg');
 }
 
-// Set default values based on device screen size
-function setDefaults(){
-  SetInputValue('#axis-slider', 6);
-  SetInputValue('#circle-diameter-slider', 100);
-  SetInputValue('#line-step-slider', 30);
-  SetInputValue('#line-gap-slider', 30);
-  SetInputValue('#stroke-weight-slider', 2);
+function GetValue(input){
+  return parseInt(input.value);
+}
+
+function initInputs(){
+  inputContainer = document.getElementById('controls');
+  axisSlider = inputContainer.querySelector('#axisSlider');
+  radiusSlider = inputContainer.querySelector('#radiusSlider');
+  circleDiameterSlider = inputContainer.querySelector('#circleDiameterSlider');
+  lineLengthSlider = inputContainer.querySelector('#lineLengthSlider');
+  lineStepSlider = inputContainer.querySelector('#lineStepSlider');
+  lineGapSlider = inputContainer.querySelector('#lineGapSlider');
+  strokeWeightSlider = inputContainer.querySelector('#strokeWeightSlider');
+
+  axisSlider.value = 6;
+  circleDiameterSlider.value = 100;
+  lineStepSlider.value = 30;
+  lineGapSlider.value = 30;
+  strokeWeightSlider.value = 2;
 
   if(windowWidth <= 1280){
-    SetInputValue('#line-length-slider', 130);
-    SetInputValue('#radius-slider', 100);
-    GetInput('#radius-slider').max = 200;
-    GetInput('#line-length-slider').max = 400;
+    lineLengthSlider.value = 130;
+    radiusSlider.value = 100;
+    radiusSlider.max = 200;
+    lineLengthSlider.max = 200;
   } else {
-    SetInputValue('#line-length-slider', 400);
-    SetInputValue('#radius-slider', 200);
-    GetInput('#radius-slider').max = 500;
+    lineLengthSlider.value = 400;
+    radiusSlider.value = 200;
+    radiusSlider.max = 500;
   }
+
+  // Hook up inputs
+  let inputs = inputContainer.getElementsByTagName('input');
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    processInput(input);
+    input.oninput = function(){
+      processInput(input);
+    }
+  }
+
+  inputContainer.querySelector('#export-button').onclick = (function () {
+    Export();
+  })
 }
 
-// Helper functions
-function SetInputValue(id, value){
-  inputContainer.querySelector(id).value = value;
-  inputContainer.querySelector(id).oninput();
-}
-
-function GetInput(id){
-  return inputContainer.querySelector(id);
-}
-
-function GetInputValue(id){
-  return parseInt(inputContainer.querySelector(id).value);
+function processInput(input){
+  // Update outputs
+  let output = inputContainer.querySelector('#' + input.id + 'Output');
+  output.value = input.value;
+  // Resize elements
+  circleElements.forEach(element => {
+    element.resize();
+  }); 
+  lineElements.forEach(element => {
+    element.resize();
+  });
+  // Update stroke weight
+  strokeWeight(GetValue(strokeWeightSlider));
 }
 
 function windowResized() {
